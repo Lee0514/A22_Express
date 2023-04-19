fe9c5e69025f0c195329b16d6ac7beab661ae8d2
Lee0514 <iris8505145@yahoo.com.tw>
2023-04-19 15:38:15 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+
+  res.render('Portfolio')
+})
+
+app.get('/contact', (req, res) => {
+
+  res.render('Contact')
+})
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
+
+
